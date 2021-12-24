e14fa3996d0436d5580a45fb3261ffc8698e9ac0
Cyril Kunjumon <cyril 2019@iiitkottayam.ac.in>
2021-12-24 20:58:41 +0530

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,49 @@
 import React from "react";
-import { AiOutlineMail } from "react-icons/ai";
+import {
+  AiOutlineMail,
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+  AiFillFacebook,
+} from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="w-screen bg-green-400 text-center text-black">
-      <div className="w-screen bg-white flex-col">
-        <p className="font-bold pt-3">CONTACT US</p>
-        <div className="h-1 w-1/12 bg-green-400 m-auto"> </div>
-        <div className="grid grid-rows-1">
-          <div>
-            <AiOutlineMail />
+      {/* contact us part of footer  */}
+      <div className="w-screen bg-white flex-col p-10">
+        {/* contact us */}
+        <p className="font-bold ">CONTACT US</p>
+        {/* Green line */}
+        <div className="h-1 w-1/12 bg-green-400 m-auto mt-2"> </div>
+        {/* Emails section */}
+        <div className="grid grid-rows-1 sm:grid-flow-col mt-5 ">
+          <div className="py-5 sm:py-0">
+            <AiOutlineMail className="m-auto text-4xl" />
+            <p>xyz@iiitkottayam.ac.in</p>
           </div>
+          <div className="py-5 sm:py-0">
+            <AiOutlineMail className="m-auto text-4xl" />
+            <p>xyz@iiitkottayam.ac.in</p>
+          </div>
+          <div className="py-5 sm:py-0">
+            <AiOutlineMail className="m-auto text-4xl" />
+            <p>xyz@iiitkottayam.ac.in</p>
+          </div>
+        </div>
+      </div>
+      {/* map comes here */}
+      <div></div>
+      {/* credits */}
+      <div className="w-screen grid grid-rows-1 sm:grid-flow-col justify-items-center items-center p-10 font-semibold">
+        {/* left side  */}
+        <div className="pb-10 sm:pb-0">
+          <p>Copyright</p>
+        </div>
+        {/* right side  */}
+        <div className="flex space-x-5 pt-10 sm:pl-16 sm:pt-0 text-2xl border-t-2  sm:border-l-4 sm:border-t-0 border-solid border-black">
+          <AiOutlineInstagram />
+          <AiOutlineTwitter />
+          <AiFillFacebook />
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,21 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-screen h-screen bg-black text-white text-center pt-56 header">
-      <p className="text-8xl font-bold ">i-CHESS 2022</p>
-      <p className="text-2xl mt-2 font-semibold">
+    <div className="w-screen h-auto sm:h-screen bg-black text-white text-center px-10 sm:px-5 pb-20 sm:pb-0 pt-32 sm:pt-56 header">
+      <p className="text-4xl sm:text-8xl xl:text-9xl font-bold ">
+        i-CHESS 2022
+      </p>
+      <p className="text-xl sm:text-2xl mt-2 font-semibold">
         IEEE International Conference on
       </p>
-      <p className="text-2xl font-semibold">
+      <p className=" text-xl sm:text-2xl font-semibold">
         Communication, Hardware and Embedded systems security
       </p>
-      <p className="mt-2 text-xl">
+      <p className="mt-2 text-lg sm:text-xl">
         September 16,17 2022 | Indian Institute of Information Technology
         Kottayam
       </p>
-      <button className="bg-green-400 text-black p-3 rounded-t-xl rounded-l-xl mt-3 text-2xl font-bold">
+      <button className="bg-green-400 text-black p-3 rounded-t-xl rounded-l-xl mt-3 text-xl sm: text-2xl font-bold">
         Learn More
       </button>
     </div>
